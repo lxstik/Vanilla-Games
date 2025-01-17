325d768e4849dc026dfa57a9479a5d1c72ffee83
Yehor <156694343+lxstik@users.noreply.github.com>
2025-01-17 18:14:16 +0000

    enrutador

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -4,17 +4,16 @@ export const header = {
     `
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="#"
-        ><img
-          src=".images/logo.svg"
-          alt=""
-          width="30"
-          height="24"
-          class="d-inline-block align-text-top"
-        />
-  
-        Vanilla Games</a
-      >
+      <a class="navbar-brand" href="#/home"
+  ><img
+    src="images/logo.svg"
+    alt=""
+    width="30"
+    height="24"
+    class="d-inline-block align-text-top router-link"
+  />
+  Vanilla Games</a
+>
       <button
         class="navbar-toggler"
         type="button"
@@ -27,32 +26,31 @@ export const header = {
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#">TOP5 Proyectos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#">A cerca de</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav ms-auto me-2 mb-2 mb-lg-0">
-          <li class="nav-item">
-            <button class="ms-2 btn btn-success">
-              Iniciar sesión
-              <i class="bi bi-box-arrow-in-right"></i>
-            </button>
-          </li>
-          <li class="nav-item">
-            <button class="ms-2 btn btn-outline-light">
-              Regístrate
-              <i class="bi bi-box-arrow-in-right"></i>
-            </button>
-          </li>
-        </ul>
-      </div>
+  <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+    <li class="nav-item">
+      <a class="nav-link active router-link" aria-current="page" href="#/home">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link router-link" aria-current="page" href="#">TOP5 Proyectos</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link router-link" aria-current="page" href="#">A cerca de</a>
+    </li>
+  </ul>
+  <ul class="navbar-nav ms-auto me-2 mb-2 mb-lg-0">
+    <li class="nav-item">
+      <a class="ms-2 btn btn-success router-link" href="#/login">
+        Iniciar sesión
+        <i class="bi bi-box-arrow-in-right"></i>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="ms-2 btn btn-outline-light router-link" href="#/registro">
+        Regístrate
+        <i class="bi bi-box-arrow-in-right"></i>
+      </a>
+    </li>
+  </ul>
     </div>
   </nav>
   
--- src/main.js
+++ src/main.js
@@ -1,25 +1,29 @@
 import { header } from './componentes/header.js';
 import { footer } from './componentes/footer.js';
+import { enrutador } from './componentes/enrutador.js';
 import 'bootstrap'
 
 import './scss/styles.scss'
 
-
-async function cargarVista() {
-  //const componente = await import('./vistas/ homeVista.js');
-  //const componente = await import('./vistas/registroVista.js') //PARA COMPROBAR MODIFICAR LA LINEA PARA QUE MANTENGA RUTA DE ARCHIVO QUE QUIERO VER
-  //const componente = await import('./vistas/proyectosVista.js');
-  //const componente = await import('./vistas/proyectoNuevoVista.js');
-  //const componente = await import('./vistas/proyectoEditarVista.js');
-  //const componente = await import('./vistas/proyectoDetalleVista.js');
-  //const componente = await import('./vistas/loginVista.js');
-  const componente = await import('./vistas/adminVista.js');
-  const vista = componente.default;
-
-  document.querySelector('main').innerHTML = vista.template;
-}
-
 document.querySelector('header').innerHTML = header.template;
 document.querySelector('footer').innerHTML = footer.template;
 
-cargarVista();
+enrutador.observadorRutas()
+window.location = '#/home'
+// async function cargarVista() {
+//   const componente = await import('./vistas/ homeVista.js');
+//   //const componente = await import('./vistas/registroVista.js') //PARA COMPROBAR MODIFICAR LA LINEA PARA QUE MANTENGA RUTA DE ARCHIVO QUE QUIERO VER
+//   //const componente = await import('./vistas/proyectosVista.js');
+//   //const componente = await import('./vistas/proyectoNuevoVista.js');
+//   //const componente = await import('./vistas/proyectoEditarVista.js');
+//   //const componente = await import('./vistas/proyectoDetalleVista.js');
+//   //const componente = await import('./vistas/loginVista.js');
+//   //const componente = await import('./vistas/adminVista.js');
+//   const vista = componente.default;
+
+//   document.querySelector('main').innerHTML = vista.template;
+// }
+
+
+
+// cargarVista();
