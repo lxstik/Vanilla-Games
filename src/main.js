16b7d5f1fe1335b77a91f8b8c0346231884f1eb8
Yehor <156694343+lxstik@users.noreply.github.com>
2025-01-17 17:38:27 +0000

    vistas

--- src/main.js
+++ src/main.js
@@ -1,25 +1,25 @@
 import { header } from './componentes/header.js';
 import { footer } from './componentes/footer.js';
-// Import all of Bootstrap's JS
 import 'bootstrap'
 
-// Import our custom CSS
 import './scss/styles.scss'
 
 
-// Función para cargar la vista principal (home)
 async function cargarVista() {
-  const componente = await import('./vistas/ homeVista.js'); // Asegúrate de que esta ruta sea correcta
-  //const componente = await import('./vistas/loginVista') PARA COMPROBAR MODIFICAR LA LINEA PARA QUE MANTENGA RUTA DE ARCHIVO QUE QUIERO VER
+  //const componente = await import('./vistas/ homeVista.js');
+  //const componente = await import('./vistas/registroVista.js') //PARA COMPROBAR MODIFICAR LA LINEA PARA QUE MANTENGA RUTA DE ARCHIVO QUE QUIERO VER
+  //const componente = await import('./vistas/proyectosVista.js');
+  //const componente = await import('./vistas/proyectoNuevoVista.js');
+  //const componente = await import('./vistas/proyectoEditarVista.js');
+  //const componente = await import('./vistas/proyectoDetalleVista.js');
+  //const componente = await import('./vistas/loginVista.js');
+  const componente = await import('./vistas/adminVista.js');
   const vista = componente.default;
 
-  // Inyectar la vista en el elemento <main>
   document.querySelector('main').innerHTML = vista.template;
 }
 
-// Inyectar el header y el footer
 document.querySelector('header').innerHTML = header.template;
 document.querySelector('footer').innerHTML = footer.template;
 
-// Cargar la vista principal
 cargarVista();
